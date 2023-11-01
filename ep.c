d2ab0ad56a179063501646c4f82faf4d7cbb76c3
Guilherme Lemes <guilhermelemes08@gmail.com>
2023-11-01 00:12:33 -0300

    Modelo final - versão 1

--- ep.c
+++ ep.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 //LÊ A QUANTIDADE DE LINHAS DE UM ARQUIVO
 int contarLinhas(char* nomeArquivo) {
@@ -43,19 +44,14 @@ int contarColunas(char* nomeArquivo){
     } 
 }
 
-// Lê o conteúdo do arquivo (caracter por caracter) e retorno uma matriz de float com os dados
-float** leArquivo (char* nomeArquivo, int quantidadeLinhas, int quantidadeColunas) { 
-    float** matriz = (float**) malloc(sizeof(float*)*quantidadeLinhas);
-
-    for (int i = 0; i < quantidadeLinhas; i++){
-        matriz[i] = (float*) malloc(sizeof(float)*quantidadeColunas);
-    }
+// Lê o conteúdo do arquivo (caracter por caracter) e retorno um array de float com os dados
+float* leArquivo (char* nomeArquivo, int quantidadeLinhas, int quantidadeColunas) { 
+    float* array = (float*) malloc(sizeof(float)*quantidadeLinhas*quantidadeColunas);
 
     float numero = 0;
     float divisor = 10.0;
     int decimal = 0;
-    int linha = 0;
-    int coluna = 0;
+    int index = 0;
 
     FILE* file = fopen(nomeArquivo, "r");
     
@@ -63,18 +59,11 @@ float** leArquivo (char* nomeArquivo, int quantidadeLinhas, int quantidadeColuna
         if (caracter == '\r') continue;
 
         if (caracter == ',' || caracter == '\n') {
-            matriz[linha][coluna] = numero;
+            array[index] = numero;
             numero = 0;
             divisor = 10.0;
             decimal = 0;
-
-            if (caracter == '\n'){
-                coluna = 0;
-                linha++;
-            }    
-
-            if (caracter == ',')
-                coluna++;
+            index++;
             
             continue;
         }
@@ -94,34 +83,81 @@ float** leArquivo (char* nomeArquivo, int quantidadeLinhas, int quantidadeColuna
     }    
 
     fclose(file);
-    return matriz;
+    return array;
 }
 
+float* knn (int k, float *xtrain, float *ytrain, float *xtest) {
+    float* distancias = (float*) malloc(sizeof(float)*576);
+    float* ytest = (float*) malloc(sizeof(float)*k);
+    int inicio = 0;
 
+    for(int index = 0; index < k; index++) {
+        for (int i = 0; i < 576; i++) {
+            float distancia = 0;
+            for (int j = inicio; j < inicio+8; j++) {
+                float operacao = xtest[j] - xtrain[(j - inicio) + i * 8];
+                distancia += (operacao * operacao);
+                //distancia += pow(xtest[j] - xtrain[(j - inicio) + i * 8], 2);
+            }
+            distancias[i] = distancia;
+        }
+        
+        float menorDistancia = distancias[0];
+        int posicaoMenor = 0;
+    
+        for (int i = 1; i < 576; i++) {
+            if (distancias[i] < menorDistancia) {
+                menorDistancia = distancias[i];
+                posicaoMenor = i;
+            }
+        }
 
-int ordena(int k, float *xtrain, float *ytrain, float *xtest) {
+        ytest[index] = ytrain[posicaoMenor];
+        inicio +=8 ;
+    }
 
+    return ytest;
+}
+
+void salvarVetorEmArquivo(char *nomeArquivo, float* vetor, int tam) {
+    FILE *arquivo = fopen(nomeArquivo, "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo para escrita.\n");
+        return;
+    }
+
+    for (int i = 0; i < tam; i++) {
+        fprintf(arquivo, "%.1f\n", vetor[i]);
+    }
+
+    fclose(arquivo);
 }
 
 int main (int argc, char** argv) {
-    char* nomeArquivo = argv[1];
-    printf("%d\n", contarLinhas(nomeArquivo));  //TESTE
-    printf("%d\n", contarColunas(nomeArquivo)); //TESTE
 
-    int linhas = contarLinhas(nomeArquivo);
-    int colunas = contarColunas(nomeArquivo);
-    float** matriz = leArquivo(nomeArquivo, linhas, colunas);
+    float* xtrain;
+    float* ytrain;
+    float* xtest;
 
-    //IMPRIME O CONTEUDO DO ARQUIVO LIDO DO ARQUIVO E ARMAZENADO EM matriz 
-    for(int i = 0; i < linhas; i++) {     
-        printf("[");
-        for(int j = 0; j < colunas; j++) {
-            printf("%f, ", matriz[i][j]);
-        }
-        printf("]\n");
-    }    
+    int linhas;
+    int colunas;
 
-    
+    for (int i = 1; i < 4; i++) {
+        linhas = contarLinhas(argv[i]);
+        colunas = contarColunas(argv[i]);
+
+        if (strcmp(argv[i], "xtrain.txt") == 0)
+            xtrain = leArquivo(argv[i], linhas, colunas);
+
+        if (strcmp(argv[i], "ytrain.txt") == 0)
+            ytrain = leArquivo(argv[i], linhas, colunas);
+
+        if (strcmp(argv[i], "xtest.txt") == 0)
+            xtest = leArquivo(argv[i], linhas, colunas);
+    }
+
+    float* ytest = knn(contarLinhas("xtest.txt"), xtrain, ytrain, xtest);
+    salvarVetorEmArquivo("ytest.txt", ytest, 192); 
 
     return 0;
 }
